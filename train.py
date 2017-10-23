b36b0997236aa9c7ca8ddbf42dc0e2384e523979
plabadille <plabadille@gmail.com>
2017-10-23 12:39:45 +0200

    add the log system to train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import sys
-import datetime
+import datetime, time
 
 import numpy as np
 from collections import defaultdict
@@ -15,14 +15,16 @@ from keras.utils import np_utils
 
 import dataset
 import net
+import logger
 
 np.random.seed(8585)
 supplied_args = sys.argv[1:]
 
 #-Constants
 ## Filesystem const
+STARTED_DATETIME = datetime.datetime.now()
 DATA_DIRECTORY = supplied_args[0] if supplied_args else "data_dir"
-MODEL_FILE_PREFIX = supplied_args[1] if len(supplied_args) >= 2 else "image-classifier_" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+MODEL_FILE_PREFIX = supplied_args[1] if len(supplied_args) >= 2 else "image-classifier_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 ## Dataset const
 N = 224 #height/width for the images : InceptionV3 model require 224
 CHANNELS = 3
@@ -77,7 +79,12 @@ def evaluate(model, vis_filename=None):
 
 #### Script ####
 
+logger.header()
+
 #-Dataset gathering and formating
+logger.log("Dataset gathering and formating", 0)
+dataset_start = time.time()
+
 X, y, tags = dataset.dataset(DATA_DIRECTORY, N)
 
 classes_count = len(tags)
@@ -91,7 +98,7 @@ X_test  = X[train_size:]
 y_test  = y[train_size:]
 Y_test = np_utils.to_categorical(y_test, classes_count)
 X, y = None, None # Performances cleanup (the value store in X are hudge)
-
+logger.execution_time(dataset_start ,"Dataset gathering and formating", 0)
 
 #- If the dataset and number of classes are low, it's recommanded to use an ImageDataGenerator.
 # @see https://keras.io/preprocessing/image/
@@ -117,10 +124,18 @@ X, y = None, None # Performances cleanup (the value store in X are hudge)
 # Then you can use it directly in the model.fit_generator (insted of model.fit) like this : generator=datagen.flow(X_train, Y_train, batch_size=BATCH_SIZE)
 
 #- Original InceptionV3 model loading
+logger.log("Original InceptionV3 model loading", 0)
+inception_start = time.time()
+
 model = net.build_model(classes_count)
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=["accuracy"])
 
+logger.execution_time(inception_start ,"Original InceptionV3 model loading", 0)
+die()
 #- Train the model on the new data for a few epochs then evaluate and save
+logger.log("Model first train, evaluation and save", 0)
+first_train_start = time.time()
+
 model.fit(
     X_train, Y_train,
     batch_size=BATCH_SIZE,
@@ -131,19 +146,39 @@ model.fit(
 evaluate(model, "000.png")
 net.save(model, tags, model_file_prefix)
 
+logger.execution_time(first_train_start ,"Model first train, evaluation and save", 0)
+
 #- Start fine-tuning convolutional layers from Inception V3
+logger.log("Fine-tuning convolutional layers from Inception V3", 0)
+finetuning_start = time.time()
 ## We freeze the first 172 layers and unfreeze the top 2 inception blocks
+logger.log("Freezing the first 172 layers and unfreeze the top 2 inception blocks", 1)
+freeze_start = time.time()
+
 for layer in model.layers[:172]:
    layer.trainable = False
 for layer in model.layers[172:]:
    layer.trainable = True
 
-## We recompile the model to take or modifications
+logger.execution_time(freeze_start, "Freezing the first 172 layers and unfreeze the top 2 inception blocks", 1)
+
+## We recompile the model to take our modifications
+logger.log("Recompiling the model to take our modifications", 1)
+compile_start = time.time()
+
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=["accuracy"]) # we use SGD with a low learning rate
 
+logger.execution_time(compile_start, "Recompiling the model to take our modifications", 1)
+
 ## We do the training
+logger.log("Second training", 1)
+second_train_start = time.time()
+
 for i in range(1,BIG_EPOCHS):
     print("mega-epoch %d/%d" % i,BIG_EPOCHS)
+    logger.log("Mega-epoch " + str(i), 2)
+    big_epoch_start = time.time()
+
     model.fit(
         X_train, Y_train,
         batch_size=BATCH_SIZE,
@@ -153,3 +188,11 @@ for i in range(1,BIG_EPOCHS):
 
     evaluate(model, str(i).zfill(3)+".png")
     net.save(model, tags, model_file_prefix)
+
+    logger.execution_time(big_epoch_start, "Mega-epoch " + str(i), 2)
+
+logger.execution_time(second_train_start, "Second training", 1)
+
+logger.execution_time(finetuning_start ,"Fine-tuning convolutional layers from Inception V3", 0)
+
+logger.footer(STARTED_DATETIME)
